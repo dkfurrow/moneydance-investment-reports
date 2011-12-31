381cb068394928194a6e16c4ad670220553500fa
dkfurrow <dkfurrow@9483eaa7-2ccd-5f5a-a690-b9910c4a215a>
2011-12-31 02:21:01 +0000

    Updated File headers, changed license to BSD. Updated meta_info.dict to reflect new release. Issue 2 now addressed.  Also Updated SecReportPanel to address issue 3. Submit build to user for testing.

--- invextension/src/com/moneydance/modules/features/invextension/JDateFieldAlt.java
+++ invextension/src/com/moneydance/modules/features/invextension/JDateFieldAlt.java
@@ -1,4 +1,25 @@
-// $Header: /home/cvs/jakarta-jmeter/src/core/org/apache/jmeter/gui/util/JDateFieldAlt.java,v 1.8 2004/02/13 02:21:38 sebb Exp $
+/* JDateFieldAlt.java
+ * Copyright 2011 Dale K. Furrow . All rights reserved.
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY EXPRESS 
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY 
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 /*
  * Copyright 2003-2004 The Apache Software Foundation.
  *
