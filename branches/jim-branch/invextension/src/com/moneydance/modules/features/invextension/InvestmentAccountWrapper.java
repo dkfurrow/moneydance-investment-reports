997653940caf4a6f1d46b64fdf500bd71eeb059b
larus@larusstone.org <larus@larusstone.org@9483eaa7-2ccd-5f5a-a690-b9910c4a215a>
2012-10-23 04:31:17 +0000

    Merge in changes from Dale's #83

--- branches/jim-branch/invextension/src/com/moneydance/modules/features/invextension/InvestmentAccountWrapper.java
+++ branches/jim-branch/invextension/src/com/moneydance/modules/features/invextension/InvestmentAccountWrapper.java
@@ -40,7 +40,7 @@ import com.moneydance.apps.md.model.SecurityType;
  * @author Dale Furrow
  * 
  */
-public class InvestmentAccountWrapper extends Aggregator implements IAccount {
+public class InvestmentAccountWrapper extends Aggregator implements IAccount, Comparable<InvestmentAccountWrapper> {
     // associated Investment Account
     private InvestmentAccount investmentAccount;
     // Account Number
@@ -281,4 +281,8 @@ public class InvestmentAccountWrapper extends Aggregator implements IAccount {
 	return this.investmentAccount;
     }
 
+    @Override
+    public int compareTo(InvestmentAccountWrapper o) {
+	return o.getName().compareTo(this.getName());
+    }
 }
