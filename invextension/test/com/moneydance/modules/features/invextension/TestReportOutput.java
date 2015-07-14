343900083ea01c6f5c4a26ad024d861651e65bf2
dkfurrow <dkfurrow@9483eaa7-2ccd-5f5a-a690-b9910c4a215a>
2015-07-14 17:36:01 +0000

    Modified some test methods to work with 2015

--- invextension/test/com/moneydance/modules/features/invextension/TestReportOutput.java
+++ invextension/test/com/moneydance/modules/features/invextension/TestReportOutput.java
@@ -47,7 +47,7 @@ public class TestReportOutput extends JFrame {
     //
     private static String testFileStr = "./resources/testMD02.moneydance";
     private static String testFileStr1 = "E:\\\\RECORDS\\moneydance\\\\Test\\\\TestCurrent.moneydance";
-    private static String testFileStr2 = "E:\\\\RECORDS\\moneydance\\\\Test\\\\testMD02.moneydance";
+    private static String testFileStr2 = "E:\\\\RECORDS\\moneydance\\\\Test\\\\20141014test-2015.moneydance";
     private static String testFileStr3 = "E:\\\\Temp\\\\TestMD02.moneydance\\\\";
     public static final File mdTestFolder = new File(testFileStr2);
 
